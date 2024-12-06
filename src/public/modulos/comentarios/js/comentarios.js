efca91128857a941cb763a7a2e78098e060bae7e
Lucas <lcssbmd@gmail.com>
2024-12-06 11:55:03 -0300

    Juntando comentários e estrelas

--- src/public/modulos/comentarios/js/comentarios.js
+++ src/public/modulos/comentarios/js/comentarios.js
@@ -17,3 +17,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".secao-de-comentario form");
   form.addEventListener("submit", handleCommentSubmit);
 });
+
+const stars = document.querySelectorAll(".stars i");
+
+function setRating(rating) {
+    stars.forEach((star, index) => {
+        if (index < rating) {
+            star.classList.add("active");
+        } else {
+            star.classList.remove("active");
+        }
+    });
+}
+const savedRating = localStorage.getItem("starRating");
+if (savedRating) {
+    setRating(parseInt(savedRating));
+}
+stars.forEach((star, index1) => {
+    star.addEventListener("click", () => {
+        setRating(index1 + 1);
+        localStorage.setItem("starRating", index1 + 1);
+    });
+});

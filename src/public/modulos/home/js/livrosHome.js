7cf58f5913a224940bbbdb6b0a14125bfad403f8
anna <annavscls123@gmail.com>
2024-12-07 22:31:32 -0300

    add imagens home, ajustando favoritos, carrossel e home

---
+++ src/public/modulos/home/js/livrosHome.js
@@ -0,0 +1,144 @@
+
+const dadosLivros =
+{
+    "ultimosLancamentos": [
+        {
+            "id": 1,
+            "titulo": "Os Ventos do Inverno",
+            "autor": "George R.R. Martin",
+            "imagem": "img/ventos-do-inverno.jpg",
+            "sinopse": "O aguardado próximo volume de *As Crônicas de Gelo e Fogo*, onde a luta pelo trono de ferro atinge novas alturas. As forças políticas e os destinos dos personagens se entrelaçam enquanto os ventos do inverno chegam ao continente de Westeros, ameaçando a estabilidade das nações e colocando todos em risco. Uma narrativa repleta de intrigas, batalhas sangrentas e reviravoltas que deixam os leitores ansiosos por mais."
+        },
+        {
+            "id": 2,
+            "titulo": "A Biblioteca da Meia-Noite",
+            "autor": "Matt Haig",
+            "imagem": "img/biblioteca-da-meia-noite.jpg",
+            "sinopse": "Nora Seed é uma mulher que, após uma série de decepções e perdas, encontra uma biblioteca mágica entre a vida e a morte. Nesse espaço, ela pode explorar diferentes versões de sua vida, onde suas escolhas se ramificam e ela vê como teria sido seu destino se tivesse feito decisões diferentes. *A Biblioteca da Meia-Noite* é uma reflexão emocionante sobre as segundas chances, o arrependimento e a importância de cada pequeno momento que define quem somos."
+        },
+        {
+            "id": 3,
+            "titulo": "Nunca vi a chuva",
+            "autor": "Stefano Volp",
+            "imagem": "img/nunca-vi-a-chuva.jpg",
+            "sinopse": "Em uma narrativa poética e sensível, *Nunca vi a chuva* conta a história de um reencontro entre dois antigos amigos, cujas vidas tomaram rumos diferentes ao longo dos anos. O livro explora temas como saudade, arrependimentos e a complexidade das relações humanas, enquanto seus personagens tentam compreender o que significa seguir em frente e aceitar o que o tempo fez com eles. Uma história tocante sobre a busca de sentido nas reviravoltas da vida."
+        },
+        {
+            "id": 4,
+            "titulo": "O pequeno caderno das coisas não ditas",
+            "autor": "Claree Poley",
+            "imagem": "img/caderno-das-coisas-nao-ditas.jpg",
+            "sinopse": "Neste livro introspectivo, a autora convida o leitor a explorar os pensamentos e sentimentos não expressos, mas que, muitas vezes, moldam nossas vidas de maneira profunda. *O pequeno caderno das coisas não ditas* é uma obra que mergulha nas camadas mais íntimas da alma humana, buscando dar voz a emoções que, por medo ou vergonha, nunca saem das páginas do coração. Uma leitura delicada e profunda sobre a comunicação não verbal e o impacto de nossas omissões."
+        },
+        {
+            "id": 5,
+            "titulo": "A morte é um dia que vale a pena viver",
+            "autor": "Claudia Quintana Arantes",
+            "imagem": "img/a-morte-e-um-dia.jpg",
+            "sinopse": "A autora reflete sobre o luto, a perda e a inevitabilidade da morte, levando o leitor a uma jornada filosófica sobre a vida e sua finitude. Em *A morte é um dia que vale a pena viver*, Claudia Quintana Arantes oferece uma abordagem sensível sobre como lidar com a dor da partida de entes queridos e o que podemos aprender com ela. O livro convida os leitores a refletir sobre o valor da vida e a importância de viver plenamente, mesmo diante da morte."
+        },
+        {
+            "id": 6,
+            "titulo": "A Dança do Tempo",
+            "autor": "Joanne Harris",
+            "imagem": "img/danca-tempo.jpg",
+            "sinopse": "Em A Dança do Tempo, Joanne Harris cria uma história envolvente que mistura magia, amor e sacrifício, ambientada em diferentes épocas e locais. A trama segue personagens que, ao longo de suas vidas, são guiados pelo tempo e pelos encontros que lhes são destinados. A autora aborda temas como destino, a importância das escolhas que fazemos e como o amor e a perda transcendem as barreiras temporais. Um romance encantador sobre as conexões profundas e misteriosas que nos ligam uns aos outros."
+        }
+    ],
+    "livrosEmAlta": [
+        {
+            "id": 7,
+            "titulo": "É Assim que Acaba",
+            "autor": "Colleen Hoover",
+            "imagem": "img/assim-que-acaba.jpg",
+            "sinopse": "Em É Assim que Acaba, Colleen Hoover explora um relacionamento turbulento e complexo, onde os limites do amor e do perdão são colocados à prova. A história segue Lily Bloom e Ryle Kincaid, enquanto enfrentam as consequências de suas escolhas e o impacto das palavras e ações em seus destinos. Um livro impactante que aborda temas como abuso, superação e a dificuldade de seguir em frente quando o amor parece impossível."
+        },
+        {
+            "id": 8,
+            "titulo": "A Hora da Estrela",
+            "autor": "Clarice Lispector",
+            "imagem": "img/hora-estrela.jpg",
+            "sinopse": "Narrado por Rodrigo S.M., o livro conta a história de Macabéa, uma jovem nordestina que vive no Rio de Janeiro. Com uma vida marcada pela pobreza e pela solidão, Macabéa busca sentido em sua existência enquanto enfrenta a indiferença do mundo ao seu redor. Uma obra comovente que reflete sobre identidade, exclusão social e a busca por um propósito."
+        },
+        {
+            "id": 9,
+            "titulo": "Sonhando com Você",
+            "autor": "Lisa Kleypas",
+            "imagem": "img/sonhando-com-vc.jpg",
+            "sinopse": "Em uma narrativa envolvente e cheia de paixão, Sara Fielding é uma jovem escritora que vive em busca de histórias para seus livros. Em um de seus passeios por Londres, ela acaba salvando Derek Craven, um empresário poderoso e sedutor que administra o clube de jogos mais exclusivo da cidade. Apesar de serem de mundos completamente diferentes, seus destinos se cruzam, dando início a uma história de amor intensa, cheia de desafios, superações e descobertas emocionantes."
+        },
+        {
+            "id": 10,
+            "titulo": "As Sombras do Passado",
+            "autor": "Tana French",
+            "imagem": "img/sombras.jpg",
+            "sinopse": "Em As Sombras do Passado, Tana French entrega um thriller psicológico repleto de suspense e reviravoltas. A história segue a detetive Antoinette Conway, enquanto ela investiga um caso de homicídio cujas pistas parecem levar a uma rede de mentiras e segredos. À medida que ela mergulha mais fundo na investigação, suas próprias sombras do passado começam a emergir, desafiando sua objetividade e colocando em risco sua própria segurança."
+        },
+        {
+            "id": 11,
+            "titulo": "A Pequena Livraria dos Sonhos",
+            "autor": "Jenny Colgan",
+            "imagem": "img/pequena-livraria.jpg",
+            "sinopse": "Ambientado em uma charmosa livraria na Escócia, *A Pequena Livraria dos Sonhos* é uma comédia romântica que conta a história de Nina, uma mulher que tenta reconstruir sua vida após enfrentar desafios pessoais. Com um toque de magia, o livro nos transporta para um ambiente acolhedor onde os livros e as histórias se tornam elementos chave para a transformação dos personagens. Uma leitura doce e leve sobre novos começos, amizades e o poder das histórias."
+        },
+        {
+            "id": 12,
+            "titulo": "Memórias Póstumas de Brás Cubas",
+            "autor": "Machado de Assis",
+            "imagem": "img/memorias.jpg",
+            "sinopse": "Narrado pelo protagonista após sua morte, este romance inovador de Machado de Assis mistura ironia, filosofia e crítica social. Brás Cubas revisita sua vida, desde sua infância privilegiada no Brasil imperial até suas reflexões sobre a sociedade, política e relações humanas. Com capítulos curtos e estilo irreverente, a obra é um marco do realismo na literatura brasileira e uma das mais influentes de todos os tempos."
+        }
+    ]
+}
+
+
+const LivrosHome = {
+    mostrarLivros: function (lista, containerId){
+        const container = document.getElementById(containerId);
+    if (!Array.isArray(lista)) {
+        console.error('A lista de livros não é um array válido.');
+        return;
+    }
+    lista.forEach(livro => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+                <a href="livro-detalhes.html?id=${livro.id}">
+                    <img src="${livro.imagem}" alt="${livro.titulo}">
+                    <h3>${livro.titulo}</h3>
+                    <p>${livro.autor}</p>
+                </a>
+            `;
+        container.appendChild(div);
+    });
+},
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.location.pathname.includes('home.html')) {
+        console.log(dadosLivros);
+            LivrosHome.mostrarLivros(dadosLivros.ultimosLancamentos, 'ultimosLancamentos');
+    } else if (window.location.pathname.includes('emAlta.html')) {
+            LivrosHome.mostrarLivros(dadosLivros.livrosEmAlta, 'livrosEmAlta');
+    }
+
+    // Carrega os detalhes do livro na página de detalhes
+    if (window.location.pathname.includes('livro-detalhes.html')) {
+        const params = new URLSearchParams(window.location.search);
+        const livroId = params.get('id');
+        const todasAsCategorias = [...dadosLivros.ultimosLancamentos, ...dadosLivros.livrosEmAlta];
+        const livro = todasAsCategorias.find(livro => livro.id == livroId);
+
+        if (!livro) {
+            // Exibe mensagem de erro ao invés de redirecionar
+            alert('Livro não encontrado. Você será redirecionado.');
+            window.location.href = '404.html'; // Redireciona para página de erro se o livro não for encontrado
+        } else {
+            document.getElementById('titulo').innerText = livro.titulo;
+            document.getElementById('autor').innerText = livro.autor;
+            document.getElementById('imagem').src = livro.imagem;
+            document.getElementById('sinopse').innerText = livro.sinopse;
+        }
+    }
+
+});
